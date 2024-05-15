9880216c9f77721c9628132ddd331094633e2b2b
Hamender Sharma <64498970+HamenderS@users.noreply.github.com>
2024-05-15 12:34:49 +0530

    Update Constant.ts
    added empty constant variable in file.

--- src/utilities/Constant.ts
+++ src/utilities/Constant.ts
@@ -1,1 +1,5 @@
 export const CHUNK_SIZE = 1024 * 1024 * 10; // 10MB, This must be bigger than or equal to 5MB, otherwise AWS will respond with: "Your proposed upload is smaller than the minimum allowed size"
+export const API_KEY = "";
+export const STAGING_URL = "";
+export const PROCESS_URL = "";
+export const STATUS_URL = "";
